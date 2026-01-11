b30d0fb713ee6341565147caa897d7589ab2a4c1
Breno <usuario@breno.local>
2026-01-11 18:53:24 -0300

    Senha para logar

--- Menus/Main.py
+++ Menus/Main.py
@@ -40,6 +40,7 @@ class Main:
         self.con = sqlite3.connect("adega.db", timeout=10, check_same_thread=False) #vou precisar arrumar essa conexão
         self.impressora = ImpressoraTxt() #mudar para impressorawindows
         self.frame_atual = None
+        self.pode_usar_atalho = False
         self.estoque = Estoque(self.con)
         self.caixa = Caixa(self.estoque, self.impressora, self.con)
         self.despesa = Despesas(self.con)
@@ -86,6 +87,11 @@ class Main:
 
         self.frame_atual = novo_frame
         self.frame_atual.grid(column=0, row=0, sticky="nsew")
+
+    def verificar_senha(self, senha, modal):
+        if senha.get() == "123":
+            modal.destroy()
+            self.pode_usar_atalho = True
     
     def voltar_menu_principal(self):
         self.trocar_frame(MenuPrincipal(self.root, self))
@@ -239,6 +245,51 @@ class MenuPrincipal(ctk.CTkFrame):
             font=("Arial", 30, "bold"),
             command=lambda: self.escolher(6)
             ).grid(column=1, row=3, padx=20, pady=20)
+        
+
+        senha = StringVar()
+
+        tela_senha = ctk.CTkToplevel(self, fg_color="#1e1e1e")
+
+        tela_senha.title("Consultar produto")
+        tela_senha.geometry("300x300")
+
+        tela_senha.transient(self)
+        tela_senha.update_idletasks()
+        tela_senha.grab_set()   
+
+        tela_senha.columnconfigure(0, weight=1)
+        tela_senha.rowconfigure((0,1), weight=1)
+        header =  ctk.CTkFrame(tela_senha, fg_color="#1e1e1e")
+        entrys =  ctk.CTkFrame(tela_senha, fg_color="#1e1e1e")
+
+        header.grid(row=0, column=0)
+
+        entrys.rowconfigure(0, weight=1)
+        entrys.columnconfigure(0, weight=1)
+        entrys.grid(row=1, column=0)
+
+        ctk.CTkLabel(header, 
+                     text="Digite a senha",
+                     font=("arial", 32, "bold")
+                     ).grid(row=0, column=0, columnspan=2)
+        
+        entry = ctk.CTkEntry(entrys,
+                         textvariable=senha,
+                         font=("Arial", 20, "bold"),
+                         width=400,
+                         height=50)
+        entry.grid(row=0, column=0)
+        entry.focus_set()
+
+
+        tela_senha.protocol("WM_DELETE_WINDOW", self.master.quit)
+
+        entry.bind("<Return>", lambda e: self.main.verificar_senha(senha, tela_senha))
+
+
+
+
 
     def escolher(self, opcao):
         """Recebe a opção escolhida,
@@ -267,7 +318,7 @@ class MenuPrincipal(ctk.CTkFrame):
         self.main.trocar_frame(escolhido(self.master, self.main))
 
     def teclas_menu(self, tecla):
-        if tecla.char in ["1", "2", "3", "4", "5", "6"]:
+        if tecla.char in ["1", "2", "3", "4", "5", "6"]and self.main.pode_usar_atalho:
             self.escolher(int(tecla.char))
 
 root = ctk.CTk()
